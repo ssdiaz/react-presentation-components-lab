3d99d5fb2514dac04f39bbf4fa82cf3ae6ee50f2
Samantha Diaz <samidiaz3@gmail.com>
2022-03-10 12:37:31 -0500

    commit tree 27a0f7be290a289a68e050ae58da060cbfddc677

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,28 @@
 // Code SimpleComponent Here
+import React, {Component} from 'react'
+
+class SimpleComponent extends Component {
+
+    state = {
+        mood: 'happy',
+    }
+
+    handleClick = () => {
+        const changeMood = this.state.mood === 'happy' ? 'sad' : 'happy'
+        
+        this.setState({
+            mood: changeMood
+        })
+    }
+
+    render() {
+        return(
+            <div onClick={this.handleClick}>
+                {this.state.mood}
+            </div>
+        )
+    }
+
+}
+
+export default SimpleComponent;
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,10 @@
 // Code SimplerComponent Here
+import React, {Component} from 'react'
+
+// class SimplerComponent extends Component {
+
+// }
+
+const SimplerComponent = props => <div onClick={props.handleClick}>I am just happy</div>
+
+export default SimplerComponent
